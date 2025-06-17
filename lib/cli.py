e03918d6d4e37019ac5816cff2c095a6d65a36ce
Sitong <nickren93@gmail.com>
2025-06-16 20:12:21 -0400

    updated cli.py ligic

--- lib/cli.py
+++ lib/cli.py
@@ -10,37 +10,44 @@ from helpers import (
 
 
 def main():
-    main_menu = True
-    while main_menu:
+    while True:
+
         menu()
         choice_1 = input("> ").upper()
+        
         if choice_1 == "E":
             exit_program()
+        
         elif choice_1 == "D":
-            list_divisions()
-            ufc_menu()
-            choice_2 = input("> ").upper()
-            if choice_2 == "E":
-                exit_program()
-            #get into division_menu, if user choose a division #
-            elif choice_2 == "1" or choice_2 == "2":
-                if find_division_by_id(int(choice_2)):
-                    list_division_fighters(choice_2)
-                    division_menu()
-                    choice_3 = input("> ").upper()
-                    #to exit the app
-                    if choice_3 == "E":
-                        exit_program() 
-                    #get into fighter_menu, if user choose a fighter #
-                    elif choice_3 == "1" or choice_3 == "2" or choice_3 == "3":
-                        if find_fighter_by_id(int(choice_2), int(choice_3)):
-                            fighter_menu()
-            #get back to the previous menu
-            elif choice_2 == "B":
-                pass
-            #if user input is invalid
-            else:
-                print("Invalid choice")
+            while True:
+                list_divisions()
+                ufc_menu()
+                choice_2 = input("> ").upper()
+
+                if choice_2 == "E":
+                    exit_program()
+
+                #get back to the previous menu
+                elif choice_2 == "B":
+                    break
+
+                #get into division_menu, if user choose a division #
+                elif choice_2 == "1" or choice_2 == "2":
+                    if find_division_by_id(int(choice_2)):
+                        list_division_fighters(choice_2)
+                        division_menu()
+                        choice_3 = input("> ").upper()
+                        #to exit the app
+                        if choice_3 == "E":
+                            exit_program() 
+                        #get into fighter_menu, if user choose a fighter #
+                        elif choice_3 == "1" or choice_3 == "2" or choice_3 == "3":
+                            if find_fighter_by_id(int(choice_2), int(choice_3)):
+                                fighter_menu()
+            
+                #if user input is invalid
+                else:
+                    print("Invalid choice")
         else:
             print("Invalid choice")
 
