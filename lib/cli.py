ab05b49d43bb9dc68a69ee58743a5182a521b1b7
Sitong <nickren93@gmail.com>
2025-06-19 02:04:02 -0400

    modulize menu into separate file

--- lib/cli.py
+++ lib/cli.py
@@ -13,6 +13,13 @@ from helpers import (
     create_new_division
 )
 
+from menus import (
+    menu,
+    ufc_menu,
+    division_menu,
+    fighter_menu
+)
+
 
 def main():
     while True:
@@ -96,42 +103,6 @@ def main():
         else:
             print("Invalid choice")
 
-
-def menu():
-    print("*******************************************")
-    print("Welcome to UFC division and fighter look up App !!")
-    print("Please select an option:")
-    print('To Exit the program, press "E"')
-    print('To shown current divisions, press "D"')
-    print("*******************************************")
-
-def ufc_menu():
-    print("*******************************************")
-    print("Please select an option:")
-    print('To Exit the program, press "E"')
-    print('To select a division, press the correspoding tag number shown above')
-    print('To add a new division to UFC, press "A"')
-    print('Go back to previous menu, press "B"')
-    print("*******************************************")
-
-def division_menu():
-    print("*******************************************")
-    print("Please select an option:")
-    print('To Exit the program, press "E"')
-    print('To select a fighter, press the correspoding tag number shown above')
-    print('To add a new fighter to this division, press "A"')
-    print('To delete this division, press "D"')
-    print('Go back to previous menu, press "B"')
-    print("*******************************************")
-
-def fighter_menu():
-    print("*******************************************")
-    print("Please select an option:")
-    print('To Exit the program, press "E"')
-    print('To update info about this fighter, press "U"')
-    print('To delete this fighter, press "D"')
-    print('Go back to previous menu, press "B"')
-    print("*******************************************")
 
 if __name__ == "__main__":
     main()
---
+++ lib/menus.py
@@ -0,0 +1,35 @@
+def menu():
+    print("*******************************************")
+    print("Welcome to UFC division and fighter look up App !!")
+    print("Please select an option:")
+    print('To Exit the program, press "E"')
+    print('To shown current divisions, press "D"')
+    print("*******************************************")
+
+def ufc_menu():
+    print("*******************************************")
+    print("Please select an option:")
+    print('To Exit the program, press "E"')
+    print('To select a division, press the correspoding tag number shown above')
+    print('To add a new division to UFC, press "A"')
+    print('Go back to previous menu, press "B"')
+    print("*******************************************")
+
+def division_menu():
+    print("*******************************************")
+    print("Please select an option:")
+    print('To Exit the program, press "E"')
+    print('To select a fighter, press the correspoding tag number shown above')
+    print('To add a new fighter to this division, press "A"')
+    print('To delete this division, press "D"')
+    print('Go back to previous menu, press "B"')
+    print("*******************************************")
+
+def fighter_menu():
+    print("*******************************************")
+    print("Please select an option:")
+    print('To Exit the program, press "E"')
+    print('To update info about this fighter, press "U"')
+    print('To delete this fighter, press "D"')
+    print('Go back to previous menu, press "B"')
+    print("*******************************************")
